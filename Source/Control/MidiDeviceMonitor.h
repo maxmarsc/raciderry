45f7f785d16a9ace908bfabf69aaac6158a5a4ff
maxmarsc <maxime.coutant@grenoble-inp.org>
2020-10-20 20:07:48 +0200

    Added class documentation to Midi monitor

--- Source/Control/MidiDeviceMonitor.h
+++ Source/Control/MidiDeviceMonitor.h
@@ -15,12 +15,28 @@
 namespace control
 {
 
+/**
+ * @class control::MidiDeviceMonitor
+ * @brief A tiny utility class which peridically watch for available MIDI 
+ * connections and automatically enable MIDI routing to the given device
+ * 
+ */
 class MidiDeviceMonitor : public juce::Timer
 {
 public:
+    /**
+     * @brief Construct a new Midi Device Monitor object
+     * 
+     * @param deviceManager The device manager to link the MIDI device to
+     */
     MidiDeviceMonitor(juce::AudioDeviceManager& deviceManager);
 
+    /**
+     * @name juce::Timer overrides.
+     */
+    ///@{
     void timerCallback() override;
+    ///@}
 
 private:
     juce::AudioDeviceManager&       m_deviceManager;
