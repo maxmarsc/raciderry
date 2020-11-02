e922239bd1a7f5378cc927d462809553a0791b1e
maxmarsc <maxime.coutant@grenoble-inp.org>
2020-11-02 19:15:41 +0100

    Added license header to Open303 files

--- Source/Engine/Filter/Open303/rosic_NumberManipulations.h
+++ Source/Engine/Filter/Open303/rosic_NumberManipulations.h
@@ -1,3 +1,29 @@
+/**
+*   This file was downloaded from the Open303 github repository
+*   
+*   https://github.com/maddanio/open303
+* 
+*   The project is licensed under the MIT License :
+
+Copyright (c) 2009 Robin Schmidt (www.rs-met.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*/
+
 #ifndef rosic_NumberManipulations_h
 #define rosic_NumberManipulations_h
 
@@ -16,7 +42,7 @@ namespace rosic
   /** Assuming, that the FPU is in 'to nearest even integer' rounding mode (which is the default),
   this function rounds to the nearest integer using upward rounding when the argument is exactly
   halfway between two integers (instead of returning the nearest even integer in this case).
-  Argument x must satify (INT_MIN/2)ñ1.0 < x < (INT_MAX/2)+1.0.  */
+  Argument x must satify (INT_MIN/2)ï¿½1.0 < x < (INT_MAX/2)+1.0.  */
   INLINE int roundToInt(double x)
   {
 #  if defined _MSC_VER
