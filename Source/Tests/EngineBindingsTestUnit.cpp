8494db7a9f53e563dbca35abb3df667ada5163a6
maxmarsc <maxime.coutant@grenoble-inp.org>
2021-01-19 15:44:28 +0100

    Added new empty test unit

---
+++ Source/Tests/EngineBindingsTestUnit.cpp
@@ -0,0 +1,28 @@
+/*
+  ==============================================================================
+
+    EngineBindingsTestUnit.cpp
+    Created: 18 Jan 2021 6:31:15pm
+    Author:  maxime
+
+  ==============================================================================
+*/
+
+#include "Tests/CustomTestUnit.h"
+#include "Tests/Utils.h"
+
+#include "Engine/Binding.h"
+
+namespace tests
+{
+
+class BindingsTestUnit : public CustomTestUnit
+{
+public:
+    BindingsTestUnit() : CustomTestUnit("Engine bindings testing", category::engine::core) {};
+
+private:
+
+}
+
+} // namespace tests
